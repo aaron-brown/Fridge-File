4a97edc39edbbb841045ad90dc998a81904c1cd6
unknown <Albie@.(none)>
2011-11-28 11:31:07 -0600

    Initial Commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+    @title = "Sign up"
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @title = @user.name
+  end
+
+end
